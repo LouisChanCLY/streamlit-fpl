67a8c5dcab494f5aca7dcbc56a3909e3373c3bcd
Louis Chan <louis.chan.cly@gmail.com>
2024-08-03 12:02:33 +0100

    feat: add model for team

--- models.py
+++ models.py
@@ -147,3 +147,27 @@ class Event(BaseModel):
     top_element: Optional[Any]
     top_element_info: Optional[Any]
     transfers_made: int
+
+
+class Team(BaseModel):
+    code: int
+    draw: int
+    form: Optional[Any]
+    id: int
+    loss: int
+    name: str
+    played: int
+    points: int
+    position: int
+    short_name: str
+    strength: int
+    team_division: Optional[Any]
+    unavailable: bool
+    win: int
+    strength_overall_home: int
+    strength_overall_away: int
+    strength_attack_home: int
+    strength_attack_away: int
+    strength_defence_home: int
+    strength_defence_away: int
+    pulse_id: int
